7605513c6882651ab1b1eceb135da290015d58dc
shrjrd <153493355+shrjrd@users.noreply.github.com>
2024-11-07 03:42:07 -0600

    Update draw.lua

--- draw.lua
+++ draw.lua
@@ -1,23 +1,25 @@
-local vec3 = Vector3.new
 local abs = math.abs
+local vec3 = Vector3.new
+local cf = CFrame.new
 local CFrameFromMatrix = CFrame.fromMatrix
+local newInstance = Instance.new
 
-local BasePart = Instance.new("Part")
+local BasePart = newInstance("Part")
 BasePart.BrickColor = BrickColor.new("Bright green")
 BasePart.CanCollide = false
 BasePart.Anchored = true
 
-local vertexPart = BasePart:Clone()
-vertexPart.Size = Vector3.new(0.1,0.1,0.1)
+local VertexPart = BasePart:Clone()
+VertexPart.Size = vec3(0.1,0.1,0.1)
 
-local wedge = Instance.new("WedgePart")
-wedge.Anchored = true
-wedge.TopSurface = Enum.SurfaceType.Smooth
-wedge.BottomSurface = Enum.SurfaceType.Smooth
+local WedgePart = newInstance("WedgePart")
+WedgePart.Anchored = true
+WedgePart.TopSurface = Enum.SurfaceType.Smooth
+WedgePart.BottomSurface = Enum.SurfaceType.Smooth
 
 local wedgeMesh = Instance.new("SpecialMesh")
 wedgeMesh.MeshType = Enum.MeshType.Wedge
-wedgeMesh.Scale = Vector3.new(0,1,1)
+wedgeMesh.Scale = vec3(0,1,1)
 
 local m = {}
 
@@ -49,12 +51,12 @@ local function drawTriangle(a, b, c, parent, w1, w2)
 	local up = bc:Cross(right).unit
 	local back = bc.unit
 	local height = abs(ab:Dot(up))
-	w1 = w1 or wedge:Clone()
+	w1 = w1 or WedgePart:Clone()
 	w1.Size = vec3(0, height, abs(ab:Dot(back)))
 	w1.CFrame = CFrameFromMatrix((a + b)/2, right, up, back)
 	wedgeMesh:Clone().Parent = w1
 	w1.Parent = parent
-	w2 = w2 or wedge:Clone()
+	w2 = w2 or WedgePart:Clone()
 	w2.Size = vec3(0, height, abs(ac:Dot(back)))
 	w2.CFrame = CFrameFromMatrix((a + c)/2, -right, up, -back)
 	wedgeMesh:Clone().Parent = w2
@@ -64,25 +66,25 @@ end
 
 local function drawLine(a,b)
 	local Part = BasePart:Clone()
-	Part.Size = Vector3.new(.025, .025, (a-b).magnitude)
-	Part.CFrame = CFrame.new((a + b) / 2, a)
+	Part.Size = vec3(.025, .025, (a-b).magnitude)
+	Part.CFrame = cf((a + b) / 2, a)
 	Part.Parent = workspace
 	return Part
 end
 
 local function drawPoint(a)
-	local Part = vertexPart:Clone()
-	Part.CFrame = CFrame.new(a)
+	local Part = VertexPart:Clone()
+	Part.CFrame = cf(a)
 	Part.Parent = workspace
 	return Part
 end
 
 function m.Polygons(csg)
-	local CSGModel = Instance.new("Model")
+	local CSGModel = newInstance("Model")
 	for _, p in ipairs(csg.polygons) do
 		local vertices, properties = p.vertices, p.shared
 		for j = 3, #vertices do
-			local TriModel = Instance.new("Model")
+			local TriModel = newInstance("Model")
 			local TriPart1, TriPart2 = drawTriangle(vertices[1].pos, vertices[j-1].pos, vertices[j].pos, TriModel)
 			if properties then
 				setProperties(TriPart1, properties) 
@@ -123,16 +125,11 @@ function m.Triangles(solid)
 	local solidparent = workspace --Instance.new("Model", workspace)
 	local vlist, ilist = solid.vlist, solid.ilist
 	for i = 1, #ilist - 2, 3 do
-		local i1 = ilist[i]
-		local i2 = ilist[i+1]
-		local i3 = ilist[i+2]
-		local v1 = vlist[i1]
-		local v2 = vlist[i2]
-		local v3 = vlist[i3] 
-		local vtx1 = Vector3.new(v1[1], v1[2], v1[3])
-		local vtx2 = Vector3.new(v2[1], v2[2], v2[3])
-		local vtx3 = Vector3.new(v3[1], v3[2], v3[3])
-		local trimodel = Instance.new("Model", solidparent)
+		local v1, v2, v3 = vlist[ilist[i]], vlist[ilist[i+1]], vlist[ilist[i+2]] 
+		local vtx1 = vec3(v1[1], v1[2], v1[3])
+		local vtx2 = vec3(v2[1], v2[2], v2[3])
+		local vtx3 = vec3(v3[1], v3[2], v3[3])
+		local trimodel = newInstance("Model", solidparent)
 		trimodel.Name = ((i - 1)/3) + 1
 		local w1, w2 = drawTriangle(vtx1, vtx2, vtx3, trimodel)
 		w1.Name = ((i - 1)/3) + 1
@@ -143,16 +140,11 @@ end
 function m.NormalsSolid(solid)
 	local vlist, ilist = solid.vlist, solid.ilist
 	for i = 1, #ilist - 2, 3 do
-		local i1 = ilist[i]
-		local i2 = ilist[i+1]
-		local i3 = ilist[i+2]
-		local v1 = vlist[i1]
-		local v2 = vlist[i2]
-		local v3 = vlist[i3] 
-		local vtx1 = Vector3.new(v1[1], v1[2], v1[3])
-		local vtx2 = Vector3.new(v2[1], v2[2], v2[3])
-		local vtx3 = Vector3.new(v3[1], v3[2], v3[3])
-		local normal = Vector3.new(v1[4], v1[5], v1[6])
+		local v1, v2, v3 = vlist[ilist[i]], vlist[ilist[i+1]], vlist[ilist[i+2]] 
+		local vtx1 = vec3(v1[1], v1[2], v1[3])
+		local vtx2 = vec3(v2[1], v2[2], v2[3])
+		local vtx3 = vec3(v3[1], v3[2], v3[3])
+		local normal = vec3(v1[4], v1[5], v1[6])
 		--local normal = (vtx2-vtx1):Cross(vtx3-vtx1).unit
 		local pos = (vtx1+vtx2+vtx3)/3
 		local part = drawLine( pos, (pos + (normal*.25)) )
@@ -161,7 +153,13 @@ function m.NormalsSolid(solid)
 end
 
 function m.VerticesSolid(solid)
-	
+	local vlist, ilist = solid.vist, solid.ilist
+	for i = 1, #ilist -2, 3 do
+		local v1, v2, v3 = vlist[ilist[i]], vlist[ilist[i+1]], vlist[ilist[i+2]] 
+		drawPoint(vec3(v1[1], v1[2], v1[3]))
+		drawPoint(vec3(v2[1], v2[2], v2[3]))
+		drawPoint(vec3(v3[1], v3[2], v3[3]))
+	end
 end
 
 m.setProperties = setProperties
